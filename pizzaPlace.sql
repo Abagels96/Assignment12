6a234231106961001422c38a7ebbc03d27056918
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2025-07-19 22:38:13 -0500

    resubmitting assignment 12

---
+++ pizzaPlace.sql
@@ -0,0 +1,129 @@
+create schema pizza_place;
+CREATE TABLE pizza_place.pizza (
+    pizza_id INT NOT NULL PRIMARY KEY,
+    pizza_type VARCHAR(100),
+    pizza_price DECIMAL(5 , 2 )
+);
+CREATE TABLE pizza_place.orders (
+    order_id INT NOT NULL PRIMARY KEY,
+    order_date DATE,
+    order_time TIME
+);
+CREATE TABLE pizza_place.customers (
+    customer_id INT NOT NULL PRIMARY KEY,
+    customer_first_name VARCHAR(100),
+    customer_last_name VARCHAR(100),
+    customer_phone_number VARCHAR(15)
+);
+CREATE TABLE pizza_place.customers_orders (
+    customer_id INT,
+    order_id INT,
+    FOREIGN KEY (customer_id)
+        REFERENCES customers (customer_id),
+    FOREIGN KEY (order_id)
+        REFERENCES orders (order_id)
+);
+CREATE TABLE pizza_place.customers_pizza (
+    customer_id INT,
+    pizza_id INT,
+    quantity INT,
+    order_id INT,
+    FOREIGN KEY (customer_id)
+        REFERENCES customers (customer_id),
+    FOREIGN KEY (pizza_id)
+        REFERENCES pizza (pizza_id)
+);
+insert into customers_orders(customer_id, order_id) 
+values 
+  (1, 1), 
+  (2, 2), 
+  (2, 3);
+insert into customers(
+  customer_id, customer_first_name, 
+  customer_last_name, customer_phone_number
+) 
+values 
+  (1, 'Trevor', 'Page', 2265554982), 
+  (2, 'John', 'Doe', 5555559498);
+insert into customers_pizza(pizza_id, customer_id, quantity,order_id) 
+values 
+  (3, 1, 2,1), 
+  (4, 1, 1,1), 
+  (1, 1, 1,1), 
+  (2, 2, 3,3), 
+  (2, 2 , 1,2),
+  (3, 2, 2,2), 
+  (4, 2, 1,3);
+  
+
+SELECT 
+    *
+FROM
+    customers_pizza;
+CREATE TABLE pizza_place.pizza_orders (
+    pizza_id INT NOT NULL,
+    order_id INT NOT NULL,
+    FOREIGN KEY (pizza_id)
+        REFERENCES pizza (pizza_id),
+    FOREIGN KEY (order_id)
+        REFERENCES orders (order_id)
+);
+insert into orders(order_id, order_date, order_time) 
+values 
+  (1, 20230910, 094700), 
+  (2, 20230910, 132000), 
+  (3, 20231010, 103700);
+insert into pizza(
+  pizza_id, pizza_type, pizza_price
+) 
+values 
+  (1, 'Pepperoni and Cheese', 7.99), 
+  (2, 'Vegetarian', 9.99), 
+  (3, 'Meat Lovers', 14.99), 
+  (4, 'Hawaian', 12.99);
+-- sum of customer1's pizza purchases
+SELECT 
+    c.customer_id,
+    customer_first_name,
+    customer_last_name,
+    SUM(cp.quantity * p.pizza_price) AS total_price
+FROM
+    customers c
+        JOIN
+    customers_pizza cp ON c.customer_id = cp.customer_id
+        JOIN
+    pizza p ON cp.pizza_id = p.pizza_id
+WHERE
+    c.customer_id = 1;
+-- sum of customer 2's pizza purchases
+SELECT 
+    c.customer_id,
+    customer_first_name,
+    customer_last_name,
+    SUM(cp.quantity * p.pizza_price) AS total_price
+FROM
+    customers c
+        JOIN
+    customers_pizza cp ON c.customer_id = cp.customer_id
+        JOIN
+    pizza p ON cp.pizza_id = p.pizza_id
+WHERE
+    c.customer_id = 2;
+  -- orders by customers and date
+SELECT 
+    o.*,
+    c.customer_id,
+    SUM(p.pizza_price * cp.quantity) AS total_price
+FROM
+    orders o
+        JOIN
+    customers_orders co ON o.order_id = co.order_id
+        JOIN
+    customers c ON co.customer_id = c.customer_id
+        JOIN
+    customers_pizza cp ON cp.customer_id = c.customer_id
+        AND cp.order_id = o.order_id
+        JOIN
+    pizza p ON cp.pizza_id = p.pizza_id
+GROUP BY o.order_date , o.order_time , c.customer_id , o.order_id;
+
